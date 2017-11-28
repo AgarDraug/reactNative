f2dfc619a528b016bafe5c61ab8576c21f3a7598
Unknown <andre.2107.silva@gmail.com>
2017-11-28 23:41:05 +0000

    finished up boilerplate

--- React-Native-4/manager/App.js
+++ React-Native-4/manager/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import firebase from 'firebase';
+import reducers from './src/reducers';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    const config = {
+      apiKey: 'AIzaSyClyduv6YRuQ_w21z-M0Vl2-Dna63NiO9E',
+      authDomain: 'manager-c590e.firebaseapp.com',
+      databaseURL: 'https://manager-c590e.firebaseio.com',
+      projectId: 'manager-c590e',
+      storageBucket: 'manager-c590e.appspot.com',
+      messagingSenderId: '415498968979'
+    };
+    
+    firebase.initializeApp(config);
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={createStore(reducers)}>
+        <View>
+          <Text>
+            Hello!
+          </Text>
+        </View>
+      </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
