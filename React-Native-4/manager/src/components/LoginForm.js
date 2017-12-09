e4a24cae848f5f44dcb67ef60fa9145eadc1f375
Unknown <andre.2107.silva@gmail.com>
2017-12-09 19:44:26 +0000

    setting up redux actions, email and passord handler, new components

---
+++ React-Native-4/manager/src/components/LoginForm.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { emailChanged, passwordChanged } from '../actions';
+import { Card, CardSection, Input, Button } from './common';
+
+class LoginForm extends React.Component {
+  onEmailChange(text) {
+    this.props.emailChanged(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChanged(text);
+  }
+
+  render() {
+    return (
+      <Card>
+        <CardSection>
+          <Input
+            label="Email"
+            placeholder="email@gmail.com"
+            onChangeText={() => this.onEmailChange()}
+            value={this.props.email}
+          />
+        </CardSection>
+        <CardSection>
+          <Input
+            secureTextEntry
+            label="Password"
+            placeholder="password"
+            onChangeText={() => this.onPasswordChange()}
+            value={this.props.password}
+          />
+        </CardSection>
+        <CardSection>
+          <Button>
+            Login
+          </Button>
+        </CardSection>
+      </Card>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    email: state.auth.email,
+    password: state.auth.password,
+  };
+};
+
+export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
